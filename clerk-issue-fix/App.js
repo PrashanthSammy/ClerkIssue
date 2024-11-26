5ed3de2065ee033ce16ac5f0d6161c661aff0dd9
prashanthsammy <dappuprashanth43@gmail.com>
2024-11-26 16:04:09 +0530

    initial commit

---
+++ clerk-issue-fix/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styled } from 'nativewind';
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
+
+export default function App() {
+  return (
+    <StyledView className="flex-1 items-center justify-center bg-white">
+      <StyledText className="text-xl font-bold text-blue-600">
+        Welcome to React Native with NativeWind!
+      </StyledText>
+      <StyledText className="mt-2 text-gray-600">
+        Start editing to see some magic happen!
+      </StyledText>
+    </StyledView>
+  );
+}
